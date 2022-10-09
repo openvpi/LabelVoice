c8b1d3286ebfba5d4869d963d888e48631979f81
Sine Striker <trueful@163.com>
2022-10-09 21:04:57 +0800

    Add LabelVoice.Widgets

---
+++ src/LabelVoice.Widgets/Views/DockCard.axaml.cs
@@ -0,0 +1,18 @@
+﻿using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace LabelVoice.Widgets.Views;
+
+public partial class DockCard : Button
+{
+    public DockCard()
+    {
+        InitializeComponent();
+    }
+
+    private void InitializeComponent()
+    {
+        AvaloniaXamlLoader.Load(this);
+    }
+}
