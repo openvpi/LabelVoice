a5c460ee16cc27eab7c4e3a66258a25a04e2a428
Fluty Deer <68263367+flutydeer@users.noreply.github.com>
2022-10-09 18:46:28 +0800

    Add items & sections panel view, add grid splitter to main window

---
+++ src/LabelVoice/Views/ItemsPanel.axaml.cs
@@ -0,0 +1,12 @@
+using Avalonia.Controls;
+
+namespace LabelVoice.Views
+{
+    public partial class ItemsPanel : UserControl
+    {
+        public ItemsPanel()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ src/LabelVoice/Views/SectionsPanel.axaml.cs
@@ -0,0 +1,12 @@
+using Avalonia.Controls;
+
+namespace LabelVoice.Views
+{
+    public partial class SectionsPanel : UserControl
+    {
+        public SectionsPanel()
+        {
+            InitializeComponent();
+        }
+    }
+}
